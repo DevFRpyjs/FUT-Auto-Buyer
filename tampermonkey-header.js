e905dcb8cebb0ecbf9f19b78f8819877e90708eb
DevFRpyjs <118846700+DevFRpyjs@users.noreply.github.com>
2022-11-22 23:29:39 +0100

    Update tampermonkey-header.js

--- tampermonkey-header.js
+++ tampermonkey-header.js
@@ -2,9 +2,9 @@ module.exports = {
   headers: {
     name: "FUT Auto Buyer",
     namespace: "http://tampermonkey.net/",
-    version: "1.5.4",
+    version: "1.0.0",
     description: "FUT Auto Buyer",
-    author: "CK Algos",
+    author: "DevFRpyjs",
     match: [
       "https://www.ea.com/*/fifa/ultimate-team/web-app/*",
       "https://www.ea.com/fifa/ultimate-team/web-app/*",
@@ -22,9 +22,9 @@ module.exports = {
     ],
     require: [
       "https://code.jquery.com/jquery-3.6.1.min.js",
-      "https://raw.githubusercontent.com/ckalgos/FUT-Auto-Buyer/main/external/discord.11.4.2.min.js",
+      "https://raw.githubusercontent.com/DevFRpyjs/FUT-Auto-Buyer/main/external/discord.11.4.2.min.js",
       "https://greasyfork.org/scripts/47911-font-awesome-all-js/code/Font-awesome%20AllJs.js?version=275337",
-      "https://github.com/ckalgos/fut-trade-enhancer/releases/latest/download/fut-trade-enhancer.user.js",
+      "https://github.com/DevFRpyjs/fut-trade-enhancer/releases/latest/download/fut-trade-enhancer.user.js",
     ],
     updateURL:
       "https://github.com/ckalgos/fut-auto-buyer/releases/latest/download/fut-auto-buyer.user.js",
